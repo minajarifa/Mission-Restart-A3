fe021fe605874cd6a67cd68c0b1e440b331c5a78
Your Name <minajmiha02@gmail.com>
2026-03-17 19:00:38 +0600

    first commit

---
+++ src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Home() {
+  return (
+    <div className="text">
+      Home
+      <button className="m-20 btn btn-secondary">Secondary</button>
+    </div>
+  );
+}
