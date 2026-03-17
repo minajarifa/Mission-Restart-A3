7075222a7e163fa9bf955034183995ce33b2ffd5
Your Name <minajmiha02@gmail.com>
2026-03-17 22:12:10 +0600

    added footer

--- src/Pages/Apps/Apps.jsx
+++ src/Pages/Apps/Apps.jsx
@@ -1,19 +1,24 @@
 import { Suspense } from "react";
 import Trendy from "../../Components/Trendy/Trendy";
 
-
 export default function Apps() {
-     const trendingPromise = fetch(`/apps.json`).then((res) => res.json());
+  const trendingPromise = fetch(`/apps.json`).then((res) => res.json());
   return (
-     <div className="my-10">
-            <h1 className="text-4xl text-center"> Our All Applications</h1>
-            <p className="text-center">
-              Explore All  Apps on the Market developed by us
-            </p>
-            <div className="flex justify-between"></div>
-            <Suspense fallback={<span className="loading loading-spinner loading-xl"></span>}>
-              <Trendy trendingPromise={trendingPromise} />
-            </Suspense>
-          </div>
-  )
+    <div className="my-10">
+      <h1 className="text-4xl text-center"> Our All Applications</h1>
+      <p className="text-center">
+        Explore All Apps on the Market developed by us
+      </p>
+      {/* TODO */}
+      {/* <div className="flex justify-between">
+        <h1>{`(${})`} Apps Found</h1>
+        <h1></h1>
+      </div> */}
+      <Suspense
+        fallback={<span className="loading loading-spinner loading-xl"></span>}
+      >
+        <Trendy trendingPromise={trendingPromise} />
+      </Suspense>
+    </div>
+  );
 }
