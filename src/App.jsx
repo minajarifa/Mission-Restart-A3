a88e75fe77dd666fc35a2e591b93c1b347142718
Your Name <minajmiha02@gmail.com>
2026-03-17 19:08:25 +0600

    added some components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import { Outlet } from 'react-router-dom'
-import './App.css'
+import { Outlet } from "react-router-dom";
+import "./App.css";
+import Header from "./Layout/Header/Header";
+import Footer from "./Layout/Footer/Footer";
 
 function App() {
   return (
     <>
-  <Outlet/>
+      <div>
+        <Header />
+        <div>
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
     </>
-  )
+  );
 }
-export default App
+export default App;
