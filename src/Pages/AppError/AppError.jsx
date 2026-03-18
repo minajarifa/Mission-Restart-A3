47aa695d727f4c0097fd7f2ea590b9716744caf1
Your Name <minajmiha02@gmail.com>
2026-03-18 09:22:48 +0600

    added 2 error page

---
+++ src/Pages/AppError/AppError.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+export default function AppError() {
+  return (
+    <div className="text-center ">
+      <div className="flex justify-center">
+        <img src="/public/App-Error.png" alt="" />
+      </div>
+      <h1 className="m-5 text-6xl">Oops!!! App not found</h1>
+      <p>The pages you are looking for is not available</p>
+      <Link
+        to={`/`}
+        className="btn bg-gradient-to-tr from-[#632EE3] to-[#9F62F2] m-5 py-5"
+      >
+        Go Back !
+      </Link>
+    </div>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,18 @@ import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./Pages/Home/Home.jsx";
 import Apps from "./Pages/Apps/Apps.jsx";
+import Error from "./Pages/Error/Error.jsx";
+import AppError from "./Pages/AppError/AppError.jsx";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App></App>,
+    errorElement:<Error></Error>,
       children: [
       {
         path: "/",
         element: <Home />,
+        errorElement:AppError,
       },
       {
         path: "/Apps",
